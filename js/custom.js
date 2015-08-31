a45e655a6fe180d0499b5e6244785025d253c2e0
Waler Garcia <wall.e.006@gmail.com>
2015-08-30 21:17:34 -0400

    completed layouts for all pages

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,16 @@
 $(document).ready( function(){
 	// alert(":)")
  $(window).ready( function(){
- 	$("#introHeader").fadeIn( 6000 );
- 	$("#introContent").fadeIn( 6000 );
+ 	$("#introHeader").fadeIn( 3000 );
+ 	$("#introContent").fadeIn( 8000 );
+ })
+ $(window).ready( function(){
+ 	$("#aboutContent").slideDown( 5000 );
+ })
+ $(window).ready( function(){
+ 	$("#resumeContent").fadeIn( 4000 );
+ })
+ $(window).ready( function(){
+ 	$("#projectFooter").fadeIn( 5000 );
  })
 })
