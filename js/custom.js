880073520753371207bc941e2366c75055d766dd
Waler Garcia <wall.e.006@gmail.com>
2015-09-10 19:23:47 -0400

    Really final draft this time. made mouseover changes to selected works page and fixed padding on about content

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,48 @@ $(document).ready( function(){
  });
 
 
-
+$("#galleryLayout").mouseover( function(){
+	$("#description").show();
+})
+$("#galleryLayout").mouseout( function(){
+    $("#description").hide();
+})
+//
+$("#galleryLayout2").mouseover( function(){
+	$("#description2").show();
+})
+$("#galleryLayout2").mouseout( function(){
+    $("#description2").hide();
+})
+//
+$("#galleryLayout3").mouseover( function(){
+	$("#description3").show();
+})
+$("#galleryLayout3").mouseout( function(){
+    $("#description3").hide();
+})
+//
+$("#galleryLayout4").mouseover( function(){
+	$("#description4").show();
+})
+$("#galleryLayout4").mouseout( function(){
+    $("#description4").hide();
+})
+//
+$("#galleryLayout5").mouseover( function(){
+	$("#description5").show();
+})
+$("#galleryLayout5").mouseout( function(){
+    $("#description5").hide();
+})
+//
+$("#galleryLayout6").mouseover( function(){
+	$("#description6").show();
+})
+$("#galleryLayout6").mouseout( function(){
+    $("#description6").hide();
+})
+//
 
 
  $(window).ready( function(){
